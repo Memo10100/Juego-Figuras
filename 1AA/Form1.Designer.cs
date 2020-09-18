5e14e91c9670873bd88b8687c98bbbdf2440a39f
juan gonzalez <juaniiiii1025@gmail.com>
2020-09-18 18:05:22 -0300

    first commit

---
+++ 1AA/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Juego_Figura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.label16, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label15, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label14, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label13, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label12, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(29, 16, 29, 16);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(612, 358);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label16.Location = new System.Drawing.Point(488, 267);
+            this.label16.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(95, 91);
+            this.label16.TabIndex = 31;
+            this.label16.Text = "h";
+            this.label16.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label16.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label15.Location = new System.Drawing.Point(335, 267);
+            this.label15.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(95, 91);
+            this.label15.TabIndex = 29;
+            this.label15.Text = "m";
+            this.label15.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label15.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label14.Location = new System.Drawing.Point(182, 267);
+            this.label14.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(95, 91);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "n";
+            this.label14.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label14.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label13.Location = new System.Drawing.Point(29, 267);
+            this.label13.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(95, 91);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "b";
+            this.label13.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label13.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label12.Location = new System.Drawing.Point(488, 178);
+            this.label12.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(95, 89);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "q";
+            this.label12.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label12.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label11.Location = new System.Drawing.Point(335, 178);
+            this.label11.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(95, 89);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "t";
+            this.label11.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label11.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label10.Location = new System.Drawing.Point(182, 178);
+            this.label10.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 89);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "d";
+            this.label10.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label10.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label9.Location = new System.Drawing.Point(29, 178);
+            this.label9.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 89);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "v";
+            this.label9.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label9.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Location = new System.Drawing.Point(488, 89);
+            this.label8.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 89);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "H";
+            this.label8.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label8.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(335, 89);
+            this.label7.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 65);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "G";
+            this.label7.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label7.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label6.Location = new System.Drawing.Point(182, 89);
+            this.label6.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 89);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "F";
+            this.label6.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label6.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label5.Location = new System.Drawing.Point(29, 89);
+            this.label5.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 89);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "E";
+            this.label5.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label5.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label4.Location = new System.Drawing.Point(488, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 89);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "D";
+            this.label4.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label4.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label3.Location = new System.Drawing.Point(335, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 89);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C";
+            this.label3.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label3.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label2.Location = new System.Drawing.Point(182, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 89);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "B";
+            this.label2.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label2.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label1.Location = new System.Drawing.Point(29, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(29, 0, 29, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 89);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            this.label1.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.label1.Click += new System.EventHandler(this.label_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(62F, 65F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(612, 358);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Margin = new System.Windows.Forms.Padding(29, 16, 29, 16);
+            this.Name = "Form1";
+            this.Text = "Juego_formas";
+            this.VisibleChanged += new System.EventHandler(this.label_Click);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
